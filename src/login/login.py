4277d647e75cd7dc92f859036038b7cfcc9f351f
sneha <patilsneha.sp04@gmail.com>
2024-01-11 15:30:24 +0530

    step 3

--- src/login/login.py
+++ src/login/login.py
@@ -4,27 +4,22 @@ from main import *
 import db_connection
 
 def login():
-    global login_page
-    login_page.tkraise()
-    login_page= Frame(root)
-    login_page.grid(row=0,column=0) 
-    login_page.configure(bg="#f0fff0")
     global username_verify
     global password_verify
-    Label(login_page, text="Login", font=("Helvetica", 12), bg="#f0fff0", fg="dark green", width=300).pack()
+    Label(login_page, text="Login", font=("Helvetica", 12), bg="light green", fg="dark green", width=300).pack()
     username_verify = StringVar()
     password_verify = StringVar()
 
-    Label(login_page,bg="#f0fff0").pack()
-    Label(login_page, text="Username : ", font=("Helvetica", 12), bg="#f0fff0", fg="dark green").pack()
+    Label(login_page,bg="light green").pack()
+    Label(login_page, text="Username : ", font=("Helvetica", 12), bg="light green", fg="dark green").pack()
     Entry(login_page, textvariable=username_verify).pack()
 
-    Label(login_page,bg="#f0fff0").pack()
-    Label(login_page, text="Password : ", font=("Helvetica", 12), bg="#f0fff0", fg="dark green").pack()
+    Label(login_page,bg="light green").pack()
+    Label(login_page, text="Password : ", font=("Helvetica", 12), bg="light green", fg="dark green").pack()
     Entry(login_page, textvariable=password_verify, show="*").pack()
     Button(login_page, text="Log-In", font=("Helvetica", 12), bg="light green", fg="dark green", command=login_verify).pack()
-    Label(login_page,bg="#f0fff0").pack()
-    
+    Label(login_page,bg="light green").pack()
+    show_frame(login_page)
 
 def login_verify():
     user_verify = username_verify.get()
@@ -42,25 +37,20 @@ def login_verify():
 def logged():
     global logg
     logg = Frame(root)
-    logg.grid() 
-    logg.configure(bg="#f0fff0")
-    Label(logg, text="Welcome {}".format(username_verify.get()), fg="green", bg="#f0fff0", font="bold").grid(row=0, column=0, columnspan=2)
-    Label(logg, bg="#f0fff0").grid(row=1, column=0)
-    Button(logg, text="Browse", height=1, font=("Helvetica", 12), bg="light green", fg="dark green", width=15).grid(row=2, column=0, columnspan=2, pady=5)
-    Button(logg, text="Log-out", font=("Helvetica", 12), bg="light green", fg="dark green", width=8, height=1, command=login_destroy).grid(row=3, column=0, columnspan=2, pady=5)
+    logg.grid(row=0,column=0,sticky="nsew") 
 
+    Label(logg, text="Welcome {}".format(username_verify.get()), fg="green",bg="#f0fff0", font="bold").pack()
+    Label(logg,bg="#f0fff0").pack()
+    Button(logg, text="Browse", height=1, font=("Helvetica", 12), bg="light green", fg="dark green", width=15).pack()
+    Button(logg, text="Log-out", font=("Helvetica", 12), bg="light green", fg="dark green", width=8, height=1, command=login_destroy).pack()
+    
 def failed():
     global fail 
     fail =Frame(root)
-    fail.grid(row=0,column=0) 
-    tk.Label(fail,text="Login").grid(row=0)
-    fail.configure(bg="#f0fff0")
-    #Label(fail, text="Invalid Credentials...", font=("Helvetica", 12), bg="#f0fff0", fg="dark green").grid(row=0, column=0, columnspan=2)
-    #Label(fail, bg="#f0fff0").grid(row=1, column=0)
-    #Button(fail, text="OK", font=("Helvetica", 12), bg="light green", fg="dark green", width=8, height=1, command=fail_destroy).grid(row=2, column=0, columnspan=2, pady=5)
-
-    Label(fail, text="Invalid Credentials...", font=("Helvetica", 12), bg="#f0fff0", fg="dark green").pack()
-    Label(fail, bg="#f0fff0").pack()
+    fail.grid(row=0,column=0,sticky="nsew") 
+    
+    Label(fail, text="Invalid Credentials...", font=("Helvetica", 12), bg="light green", fg="dark green").pack()
+    Label(fail, bg="light green").pack()
     Button(fail, text="OK", font=("Helvetica", 12), bg="light green", fg="dark green", width=8, height=1, command=fail_destroy).pack()
 
 def fail_destroy():
--- src/main.py
+++ src/main.py
@@ -2,21 +2,31 @@ from tkinter import *
 import tkinter as tk
 import registration
 import login
+def show_frame(frame):
+    frame.tkraise()
+
 def main_screen():
-    global root,main_page
+    global root,main_page,login_page,reg_page
     root = tk.Tk()
+    root.rowconfigure(0,weight=1)
+    root.columnconfigure(0,weight=1)
     root.title("Login Portal")
     root.geometry("300x300")
-    root.configure(bg="#f0fff0")
-    main_page=Frame(root)
-    main_page.pack()
-    main_page.configure(bg="#f0fff0")
+
+    main_page = tk.Frame(root)
+    reg_page = tk.Frame(root)
+    login_page = tk.Frame(root)
+
+    for page in (main_page, reg_page, login_page):
+        page.grid(row=0,column=0,sticky='nsew')
+
     Label(main_page, text="Welcome to the login portal", font=("Helvetica", 12), bg="#f0fff0", fg="dark green", width=30).pack()
     Label(main_page,bg="#f0fff0").pack()
-    Button(main_page, text="Log-In", height=1, font=("Helvetica", 12), bg="light green", fg="dark green", width=8, command=login).pack()
+    Button(main_page, text="Log-In", height=1, font=("Helvetica", 12), bg="light green", fg="dark green", width=8,command=lambda:show_frame(login_page)).pack()
     Label(main_page,bg="#f0fff0").pack()
     Button(main_page, text="Registration", height=1, font=("Helvetica", 12), bg="light green", fg="dark green", width=15, command=lambda:registration.tkraise()).pack()
     Label(main_page,bg="#f0fff0").pack()
+    show_frame(main_page)
     root.mainloop()
 
 if __name__ == "__main__":
